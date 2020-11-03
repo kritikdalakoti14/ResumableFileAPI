556ce6e95337d7daeafe2180d75678c5cb7b4af0
kritikdalakoti14 <kd.ecerasystem@gmail.com>
2020-11-03 22:10:55 +0530

    adding ignore files

---
+++ File/ignore.js
@@ -0,0 +1,2 @@
+// ignore this file 
+// in this directory the uploaded files would be kept here after the upload
---
+++ temp/ignore.js
@@ -0,0 +1,2 @@
+// ignore this file 
+// in this directory the uploaded files would be kept here temporary till the upload is done and then moved to file directory
